59515c84f83298ee4d9bf3694d36fb67b1a71650
2002hk <hrutujakalaskar@gmail.com>
2024-12-02 21:58:38 +0530

    completed forms

--- forms.py
+++ forms.py
@@ -14,3 +14,40 @@ from wtforms.validators import(
     Optional,
     EqualTo
     )
+class SignupForm(FlaskForm):
+    username=StringField(
+        "username",
+        validators=[DataRequired(),Length(2,30)]
+    )
+    email=StringField(
+        "Email",
+        validators=[DataRequired(),Email()]
+    )
+    dob=DateField(
+        "Date of Birth",
+        validators=[Optional()]
+    )
+    Gender=SelectField(
+        "Gender",
+        validators=["Male","Female","Other"]
+    )
+    password=PasswordField(
+        "Password",
+        validators=[DataRequired(),Length(5,30)]
+    )
+    confirm_password=PasswordField(
+        "Password",
+        validators=[DataRequired(),Length(5,30),EqualTo("password")]
+    )
+    submit=SubmitField("Sign Up")
+class LoginForm(FlaskForm):
+    email=StringField(
+        "Email",
+        validators=[DataRequired(),Email()]
+    )
+    password=PasswordField(
+        "Password",
+        validators=[DataRequired(),Length(5,25)]
+    )
+    remember_me=BooleanField("Remember Me")
+    submit=SubmitField("Login")
